72e70491c2759f25e31baaa1c84379f690883f9f
Lakshmiprathap Vadde <lakshmiprathap.v@datasemantics.in>
2025-02-07 11:51:11 +0530

    ssupporting doc URL changes

--- Serina-UI/serinaBase/src/app/services/shared.service.ts
+++ Serina-UI/serinaBase/src/app/services/shared.service.ts
@@ -520,7 +520,7 @@ export class SharedService {
     });
   }
   downloadSupportDoc(doc_name) {
-    return this.http.get(`${this.apiUrl}/${this.apiVersion}/Invoice/DownloadSupportingDocument/${this.userId}/idInvoice/${this.invoiceID}?file_name=${doc_name}`, { responseType: 'blob' }).pipe(retry(3));
+    return this.http.get(`${this.apiUrl}/${this.apiVersion}/Invoice/DownloadSupportingDocument/${this.userId}/${this.invoiceID}?file_name=${doc_name}`, { responseType: 'blob' }).pipe(retry(3));
   }
   deleteSupport(filename){
     return this.http.get(`${this.apiUrl}/${this.apiVersion}/Invoice/deleteSupportingDocument/${this.userId}/idDocument/${this.invoiceID}?file_name=${filename}`).pipe(retry(3));
